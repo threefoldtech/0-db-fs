ce830d6ea37711573358306ea83ed7108e6ef104
Maxime Daniel <root@maxux.net>
2021-05-18 03:25:52 +0200

    version: bump to v0.1.6

--- src/zdbfs.h
+++ src/zdbfs.h
@@ -1,7 +1,7 @@
 #ifndef ZDBFS_H
     #define ZDBFS_H
 
-    #define ZDBFS_VERSION  "0.1.5"
+    #define ZDBFS_VERSION  "0.1.6"
 
     // internal version used for metadata header
     #define ZDBFS_INTERNAL_VERSION  1
